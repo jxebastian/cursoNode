60c359e59a3fcb26fdc2702dece98f1a07e70190
Johan <josrodriguezca@unal.edu.co>
2019-03-23 20:38:35 -0500

    edit

--- src/app.js
+++ src/app.js
@@ -10,9 +10,9 @@ const directorioPublico = path.join(__dirname, '../public');
 const directorioPartials = path.join(__dirname, '../partials');
 app.use(express.static(directorioPublico));
 hbs.registerPartials(directorioPartials);
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const dirNode_modules = path.join(__dirname , '../node_modules')
+const dirNode_modules = path.join(__dirname, '../node_modules')
 app.use('/css', express.static(dirNode_modules + '/bootstrap/dist/css'));
 app.use('/js', express.static(dirNode_modules + '/jquery/dist'));
 app.use('/js', express.static(dirNode_modules + '/popper.js/dist'));
@@ -24,24 +24,21 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-console.log(funciones.obtenerUsuarios());
-
 app.get('/roles-usuarios', (req, res) => {
   res.render('roles-usuarios', {
     lista: funciones.obtenerUsuarios()
   });
 });
 
-let cursos = funciones.obtenerCursos();
-let cursosDisponibles = cursos.filter(curso => curso.estado == "disponible");
-
 app.get('/cursos', (req, res) => {
+  let cursos = funciones.obtenerCursos();
+  let cursosDisponibles = cursos.filter(curso => curso.estado == "disponible");
   res.render('cursos', {
     lista: cursosDisponibles
   });
 });
 
 app.listen(3000, () => {
-    console.log('Escuchando por el puerto 3000');
+  console.log('Escuchando por el puerto 3000');
 
 });
