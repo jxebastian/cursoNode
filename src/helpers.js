8a74cd15ca76bc89896965b739ae21fdbd896090
fcortesn <37152398+fcortesn@users.noreply.github.com>
2019-03-25 21:02:10 -0500

    .

--- src/helpers.js
+++ src/helpers.js
@@ -7,9 +7,10 @@ hbs.registerHelper('listarCursos', (cursos) => {
     let cabecera = curso.nombre + "<br>" +
                     curso.descripcion + "<br>" +
                     curso.valor + " pesos.";
-    let contenido = curso.descripcion + "<br>" +
-                    "Modalidad " + curso.modalidad + "<br>" +
-                    "Intensidad horaria " + curso.intensidad + " horas.";
+    let contenido = "<b> Descripción: </b> " + curso.descripcion + "<br>" +
+                    "<b> Modalidad: </b> " + curso.modalidad + "<br>" +
+                    "<b> Intensidad horaria: </b> " + curso.intensidad + " horas." + "<br>"  +
+                    "<b> Valor: </b> " + curso.valor + " pesos.";
     texto = texto +
             `<div class="card">
               <div class="card-header" id="heading${i}">
@@ -28,40 +29,5 @@ hbs.registerHelper('listarCursos', (cursos) => {
             </div>`;
             i = i + 1;
   });
-  texto = texto + "</div>";
-  texto = texto + `<div class="row">
-                    <div class="col-4">
-                      <div class="list-group" id="list-tab" role="tablist">`;
-  i = 1;
-  cursos.forEach(curso => {
-    if (i == 1) {
-      active = "active";
-    } else {
-      active = "";
-    }
-    texto = texto + `<a class='list-group-item list-group-item-action ${active}' id='list-home-${i}' data-toggle='list' href='#list-${i}' role='tab' aria-controls='home'>${curso.nombre}</a>`;
-    i = i + 1;
-  });
-  texto = texto + ` </div>
-                  </div>
-                  <div class="col-8">
-                    <div class="tab-content" id="nav-tabContent">`;
-  i = 1;
-  cursos.forEach(curso => {
-    if (i == 1) {
-      active = "show active";
-    } else {
-      active = "";
-    }
-    let contenido = curso.descripcion + "<br>" +
-                    "Valor: " + curso.valor + "<br>" +
-                    "Modalidad " + curso.modalidad + "<br>" +
-                    "Intensidad horaria " + curso.intensidad + " horas.";
-    texto = texto + `<div class='tab-pane ${active}' id='list-${i}' role='tabpanel' aria-labelledby='list-home-${i}'>${contenido}</div>`;
-    i = i + 1;
-  });
-  texto = texto + `</div>
-                </div>
-              </div>`;
   return texto;
 });
