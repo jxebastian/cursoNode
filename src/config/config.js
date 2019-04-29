e823d99fdf8ebc8d81e880a34bacc39869abd89c
jxebastian <jxebastian@users.noreply.github.com>
2019-04-29 10:55:57 -0500

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,3 +1,3 @@
 process.env.PORT = process.env.PORT || 3000;
 process.env.URLDB = 'mongodb://admin:a12345@ds013222.mlab.com:13222/curso_node'
-process.env.SENDGRID_API_KEY = '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'
+process.env.SENDGRID_API_KEY = '' 
